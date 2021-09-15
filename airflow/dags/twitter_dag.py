f0d22d3b72ceacf405942fdab5d940bc73de7193
Alexsander Caproni <alexsandercaproni@192.168.1.3>
2021-09-14 23:53:55 -0300

    Aprimorando DAG e inserindo args

--- airflow/dags/twitter_dag.py
+++ airflow/dags/twitter_dag.py
@@ -4,35 +4,74 @@ from airflow.plugins.operator.twitter_operator import TwitterOperator
 
 from datetime import datetime
 from os.path import join
+from pathlib import Path
 
 from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
 from airflow.models import DAG
+from airflow.utils.dates import days_ago
 
 
-with DAG(dag_id='twitter_dag', start_date=datetime.now()) as dag:
+ARGS = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": days_ago(6),
+}
+
+BASE_FOLDER = join(
+    str(Path("~/Documents").expanduser()),
+    "python_projects/twitter_data_pipeline/datalake/{stage}/neo_tweets/{partition}"
+)
+
+SPARK_SCRIPT_FOLDER = join(
+    str(Path("~/Documents").expanduser()),
+    "python_projects/twitter_data_pipeline/spark"
+)
+
+PARTITION_FOLDER = "extract_date={{ ds }}"
+TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.00Z"
+
+
+with DAG(
+    dag_id = "twitter_dag", 
+    default_args=ARGS,
+    schedule_interval="0 9 * * *",
+    max_active_runs=1
+) as dag:
     twitter_operator = TwitterOperator(
-        task_id = 'twitter_neo',
-        query='Neoenergia',
+        task_id = "twitter_neo",
+        query = "Neoenergia",
         file_path=join(
-            '/Users/alexsandercaproni/Documents/python_projects/twitter_data_pipeline/datalake/bronze',
-            'neo_tweets',
-            'extract_date={{ ds }}',
-            'Neoenergia_{{ ds_nodash }}.json'
+            BASE_FOLDER.format(stage="bronze", partition=PARTITION_FOLDER),
+            "Neoenergia_{{ ds_nodash }}.json"
+        ),
+        start_time=(
+            "{{"
+            f" execution_date.strftime('{ TIMESTAMP_FORMAT }') "
+            "}}"
+        ),
+        end_time=(
+            "{{"
+            f" next_execution_date.strftime('{ TIMESTAMP_FORMAT }') "
+            "}}"
         )
     )
 
     twitter_transform = SparkSubmitOperator(
         task_id="transform_twitter_neo",
-        application=(
-            "/Users/alexsandercaproni/Documents/python_projects/twitter_data_pipeline/spark/twitter_transformation.py"
+        application=join(
+            SPARK_SCRIPT_FOLDER,
+            "twitter_transformation.py"
         ),
         name="twitter_transformation",
         application_args=[
             "--src",
-            "/Users/alexsandercaproni/Documents/python_projects/twitter_data_pipeline/datalake/bronze/neo_tweets/extract_date=2021-08-29",
+            BASE_FOLDER.format(stage="bronze", partition=PARTITION_FOLDER),
             "--dest",
-            "/Users/alexsandercaproni/Documents/python_projects/twitter_data_pipeline/datalake/silver/neo_tweets",
+            BASE_FOLDER.format(stage="silver", partition=""),
             "--process-date",
-            "{{ ds }}"
+            "{{ ds }}",
         ]
     )
+
+    # Criando dependencia entre as tasks
+    twitter_operator >> twitter_transform
